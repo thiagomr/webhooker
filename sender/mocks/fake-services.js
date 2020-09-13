d68572a5e475aade84a03b913b3db38c7d181a7a
Thiago Moraes <thiagodmor@gmail.com>
2020-09-13 14:55:56 -0300

    update tests

--- sender/mocks/fake-services.js
+++ sender/mocks/fake-services.js
@@ -7,5 +7,9 @@ module.exports.logger = {
 module.exports.broker = {
     ack(queue, message) {
         console.log('ack message', queue, message);
+    },
+
+    sendMessage(queue, message) {
+        console.log('send message', queue, message);
     }
 };
